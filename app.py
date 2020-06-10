58c8a02ff32715e24eee66a91b183486c19a3595
Koushik707 <60656940+Koushik707@users.noreply.github.com>
2020-06-11 00:39:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,16 @@ import time
 st.title("Flower Classification")
 
 try:
-	uploaded_file = st.file_uploader("Choose an image of Daisy, Dandellion, Rose, 		Sunflower, or Tulip.....", 			type=["jpg","jpeg","png"])
+	uploaded_file = st.file_uploader("Choose an image of Daisy, Dandellion, Rose,Sunflower, or Tulip.....", 			type=["jpg","jpeg","png"])
 
 	classes = ["Daisy","Dandelion","Rose","Sunflower","Tulip"]
 	
 	if uploaded_file is not None:
 	    TARGET_SIZE = (224, 224)			
 	    uploaded = Image.open(uploaded_file)
-	    time.sleep(5)
-	    actual_class = st.radio("What is the actual class?", classes)
 	    model = load_model('flower.h5')
 	    st.image(uploaded, caption = 'image uploaded...', use_column_width = True)
+	    actual_class = st.radio("What is the actual class?", classes)
 	    if st.button('Predict Class'):
 	    	img = np.array(uploaded.resize(TARGET_SIZE))/255
 	    	img = np.expand_dims(img, axis = 0)
