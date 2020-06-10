9be78b40765cb83fc085bcc3b0c5e58d69b86054
Koushik707 <60656940+Koushik707@users.noreply.github.com>
2020-06-10 21:24:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,24 +3,24 @@ from PIL import Image
 from tensorflow.keras.models import load_model
 import numpy as np
 
-st.title("Dog Cat Classification")
+st.title("Flower Classification")
 
 try:
-	uploaded_file = st.file_uploader("Choose an image of dog or cat...", type=["jpg","jpeg","png"])
+	uploaded_file = st.file_uploader("Choose an image of Daisy, Dandellion, Rose, 		Sunflower, or Tulip.....", 			type=["jpg","jpeg","png"])
+
+	classes = ["Daisy","Dandelion","Rose","Sunflower","Tulip"]
 
 	if uploaded_file is not None:
 	 	
 	    TARGET_SIZE = (224, 224)			
 	    uploaded = Image.open(uploaded_file)	
-	    model = load_model('DogCatClassifier.h5')
+	    model = load_model('flower.h5')
 	    st.image(uploaded, caption = 'image uploaded...', use_column_width = True)
 	    if st.button('Predict CLass'):
 	    	img = np.array(uploaded.resize(TARGET_SIZE))/255
 	    	img = np.expand_dims(img, axis = 0)
-	    	result = int((model.predict(img)[0][0]) > 0.5)
-	    	if result:
-	    		st.write('This is a dog')
-	    	else:
-	    		st.write('This is a cat')
+	    	result = classes[model.predict(img)[0].argmax()]
+	    	
+	    	st.write("This is a "+result)
 except:
-	st.write("Corrupted Image!!...please try another")
+	st.write("Corrupted image!!!...Plz try another")
